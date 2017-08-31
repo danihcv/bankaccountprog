0415b129d1dae9b509baf16522a7cc5eedbc8c35
chernojallow <31118509+chernojallow@users.noreply.github.com>
2017-08-30 22:45:28 -0500

    Update BankAccountProg.java

--- BankAccountProg.java
+++ BankAccountProg.java
@@ -7,21 +7,31 @@ public static void main(String[] args){
 	//Main code here 
 	System.out.println("Welcome to your account");
 	}
-
-
+  private double balance;
+   public BankAccountProg(){
+   }
+   public BankAccountProg ( double balance){
+	   this.balance = balance;
+	   
 
 //This function will deposit money into an account
-public void depositM(){
-
-
+public void depositM(double amount){
+   balance +=amount;
+ 
 
 	}
 
 //This function will withdraw money from the account
-public void withdrawM(){
-
+public void withdrawM(double amount){
+      balance-=amount
 
 	}
+	
+	BankAccountProg bal = new BankAccountProg ();
+	bal.depositM();
+	bal.withdrawM();
+	
+	
 }
 
 
